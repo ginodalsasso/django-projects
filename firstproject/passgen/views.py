8288d4b93931e12ae166687ec1056bf0a6137141
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-11-09 16:04:58 +0100

    ADD function to generate password with views

--- firstproject/passgen/views.py
+++ firstproject/passgen/views.py
@@ -4,15 +4,25 @@ import random
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello, World!')
+    return render(request,'home.html')
 
 def password(request):
     characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    
+    if request.GET.get('digits'):
+        characters.extend(list('0123456789'))
+
+    if request.GET.get('symbols'):
+        characters.extend(list('!@#$%^&*()'))
+
+    length = int(request.GET.get('length', 10))
+
     password = ""
 
-    for x in range(15): # Generate a 10-character password
+    for x in range(length): # Loop through the length of the password
         password += random.choice(characters) # Add a random character to the password
-    
-    result = {'password': password}
 
-    return JsonResponse(result)
+    return render(request, 'password.html', {'password': password})
